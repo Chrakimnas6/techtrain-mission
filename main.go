1c619b146494af64770148e51926d337645130db
hao.a.chen <hao.a.chen@rakuten.com>
2022-02-03 00:18:44 +0900

    finished POST

--- main.go
+++ main.go
@@ -6,18 +6,61 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 )
 
+type User struct {
+	ID    uint   `gorm:"AUTO_INCREMENT"`
+	Name  string `json:"name"`
+	Token string
+}
+
 func main() {
 	db := sqlConnect()
+	db.AutoMigrate(&User{})
 	defer db.Close()
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
+		db := sqlConnect()
+		var users []User
+		db.Find(&users)
+		defer db.Close()
+
+		c.HTML(200, "index.html", gin.H{
+			"users": users,
+		})
+	})
+
+	router.POST("/user/create", func(c *gin.Context) {
+		db := sqlConnect()
+		var user User
+		c.BindJSON(&user)
+		// //generate hash
+		// h := sha1.New()
+		// s := user.Name + time.Now().String()
+		// fmt.Println(s)
+		// h.Write([]byte(s))
+		// token := h.Sum(nil)
+		token := uuid.New()
+
+		user.Token = string(token.String())
+
+		fmt.Println("create user " + user.Name + " with token " + user.Token)
+		db.Create(&user)
+
+		//redirect to the home page
+		c.Redirect(302, "/")
+	})
+
+	router.GET("/user/get", func(c *gin.Context) {
+		db := sqlConnect()
+		defer db.Close()
+		//token := c.GetHeader("x-token")
+
 	})
 
 	router.Run()
@@ -46,13 +89,13 @@ func sqlConnect() (database *gorm.DB) {
 			count++
 			if count > 180 {
 				fmt.Println("")
-				fmt.Println("DB接続失敗")
+				fmt.Println("Connection failed")
 				panic(err)
 			}
 			db, err = gorm.Open(DBMS, CONNECT)
 		}
 	}
-	fmt.Println("DB接続成功")
+	fmt.Println("Connection succeeded")
 
 	return db
 }
